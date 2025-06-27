545a0e4a010a6953be16386ede113385ad6e5e0d
Nandan222001 <nandankumar222001@gmail.com>
2025-06-27 15:01:28 +0530

    Models Creations

---
+++ models/Users.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const userSchema = new mongoose.Schema({
+    firstName : {
+        type : String,
+        required : true ,
+        trim : true ,
+    },
+    lastName : {
+        type : String ,
+        required : true ,
+        trim : true ,
+    },
+    email : {
+        type : String ,
+        required : true ,
+        trim : true ,
+    },
+    contact_number : {
+        type : String ,
+        required : true ,
+        trim : true ,
+    },
+    password : {
+        type : String,
+        required : true ,
+        trim : true ,
+    },
+    accountType : {
+        type : String,
+        enum : ["Student","Admin","Instructor"],
+        required : true ,
+    },
+    active : {
+        type : Boolean,
+        default : true ,
+    },
+    additionalDetails : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        ref : "Profile"
+    },
+    courses : [
+        {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true,
+        ref : "Course"
+        }
+    ],
+    image : {
+        type : String ,
+        required : true ,
+    },
+    courseProgress : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "CourseProgress"
+        },
+    ],
+
+});
+
+module.exports = mongoose.model('User',userSchema);
