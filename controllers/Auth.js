7e911996ddb00dd455b14178b293765ace7801a3
Nandan222001 <nandankumar222001@gmail.com>
2025-07-01 17:22:51 +0530

    Login Function Created

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -3,6 +3,7 @@ const OTP = require('../models/OTP');
 const otpGenerator = require('otp-generator');
 const bcrypt = require('bcrypt');
 const Profile = require('../models/Profile');
+const jwt = require('jsonwebtoken');
 
 const sentOtp = async(req,res) => {
     
@@ -59,81 +60,151 @@ const sentOtp = async(req,res) => {
 //sendOtp
 
 const signUp = async(req,res) => {
+    try {
+            const {
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword,
+            accountType,
+            contactNumber,
+            otp
+        } = req.body;
 
-    const {
-        firstName,
-        lastName,
-        email,
-        password,
-        confirmPassword,
-        accountType,
-        contactNumber,
-        otp
-    } = req.body;
+        if(!firstName || !lastName || !email || !password || !confirmPassword || contactNumber || otp ) {
+            return res.status(403).json({
+                success : false , 
+                message : "All fields are required !!!"
+            });
+        }
 
-    if(!firstName || !lastName || !email || !password || !confirmPassword || contactNumber || otp ) {
-        return res.status(403).json({
-            success : false , 
-            message : "All fields are required !!!"
+        const checkEmailExist = await User.findOne({email});
+
+        if(checkEmailExist) {
+            return res.status(403).json({
+                success : false,
+                message : "User Allready Exist !!!"
+            });
+        }
+
+        if(password !== confirmPassword) {
+            return res.status(403).json({
+                success : false,
+                message : "Password And Confirm Password is not equal !!!"
+            });
+        }
+
+        const recentOtp = await otp.findOne({email}).sort({createdAt:-1}).limit(1);
+
+        if(recentOtp.length == 0) {
+            return res.status(400).json({
+                success : false ,
+                message : "Otp Not found !!!",
+            });
+        } else if(recentOtp.otp !== otp){
+            return res.status(400).json({
+                success : false ,
+                message : "Invalid Otp !!!"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        const profileDetails = await Profile.create({
+            gender : null,
+            dateOfBirth : null ,
+            about : null ,
+            contactNumber : null ,
         });
-    }
 
-    const checkEmailExist = await User.findOne({email});
-
-    if(checkEmailExist) {
-        return res.status(403).json({
-            success : false,
-            message : "User Allready Exist !!!"
+        const user = await User.create({
+            firstName,
+            lastName,
+            email,
+            contactNumber ,
+            password : hashedPassword ,
+            accountType ,
+            additionalDetails : profileDetails._id,
+            image : `https://api.dicebear.com/9.x/initials/svg/seed=${firstName} ${lastName}`
         });
-    }
 
-    if(password !== confirmPassword) {
-        return res.status(403).json({
-            success : false,
-            message : "Password And Confirm Password is not equal !!!"
-        });
-    }
-
-    const recentOtp = await otp.findOne({email}).sort({creaatedAt:-1}).limit(1);
-
-    if(recentOtp.length == 0) {
-        return res.status(400).json({
+        res.status(200).json({
+            success : true ,
+            data : user ,
+            message : "User Created Succesfully !!!"
+        })
+    } catch (error) {
+        res.status(500).json({
             success : false ,
-            message : "Otp Not found !!!",
-        });
-    } else if(recentOtp.otp !== otp){
-        return res.status(400).json({
-            success : false ,
-            message : "Invalid Otp !!!"
-        });
+            message : "Internal Server Error !!!"
+        })
     }
-
-    const hashedPassword = await bcrypt.hash(password,10);
-
-    const profileDetails = await Profile.create({
-        gender : null,
-        dateOfBirth : null ,
-        about : null ,
-        contactNumber : null ,
-    });
-
-    const user = await User.create({
-        firstName,
-        lastName,
-        email,
-        contactNumber ,
-        password : hashedPassword ,
-        accountType ,
-        additionalDetails : profileDetails._id,
-        image : `https://api.dicebear.com/9.x/initials/svg/seed=${firstName} ${lastName}`
-    });
-
-    res.status(200).json({
-        success : true ,
-        data : user ,
-        message : "User Created Succesfully !!!"
-    })
 }
-//signUp
+//signUp function
 
-exports.module = {sentOtp , signUp };
+const login = async(req,res) => {
+    
+    try {
+
+        const {email,password} = req.body;
+
+        if(!email || !password) {
+            return res.status(403).json({
+                success : false ,
+                message : "All fields are required !!!"
+            });
+        }
+
+        const userExist = await User.findOne({email}).populate('additionalDetails');
+
+        if(!userExist) {
+            return res.status(400).json({
+                success : false ,
+                message : "User Doesnt Exist Kindly Sign Up first !!!"
+            });
+        }
+
+        if(await bcrypt.compare(password,userExist.password)) {
+
+            const payload = {
+                id : userExist._id,
+                email : userExist.email,
+                role : userExist.accountType
+            }
+
+            const token = jwt.sign(payload , process.env.JWT_SECRET_TOKEN,{
+                expiresIn : "2h",
+            });
+
+            userExist.token = token;
+            userExist.password = undefined;
+
+            const options = {
+                expires : new Date(Date.now() * 3 * 24 * 60 * 60 * 100),
+                httpOnly : true
+            };
+
+            res.cookie("token",token,options).status(200).json({
+                success : true,
+                token,
+                userExist,
+                message : "Logged in Succesfully !!!"
+            });
+
+        } else {
+            return res.status(401).json({
+                success : false , 
+                message : "Incorrect Password !!!",
+            });
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            success : false ,
+            message : "Internal Server Error !!!"
+        })
+    }
+}
+//login
+exports.module = {sentOtp , signUp , login};
