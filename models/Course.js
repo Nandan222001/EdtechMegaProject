caa4b7e3125ceb74d995d349e5dc4972984f33fb
Nandan222001 <nandankumar222001@gmail.com>
2025-07-11 14:54:01 +0530

    Razorpay Payment Setup Start

--- models/Course.js
+++ models/Course.js
@@ -38,8 +38,12 @@ const courseSchema = new mongoose.Schema({
         type : String
     },
     tag : {
+        type : [String],
+        required : true
+    },
+    category : {
         type : mongoose.Schema.Types.ObjectId,
-        ref : 'Tag'
+        ref : "Category"
     },
     studentsEnrolled : [
         {
